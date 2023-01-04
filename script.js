bf85913aa2740cf4e31b95ece338d7f7316f4b16
Caio Bezerra <68752886+caiobss@users.noreply.github.com>
2023-01-03 23:30:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('.hamburguer').addEventListener("click", ()=>{
+   document.querySelector('body').classList.toggle('showsidebar')
+})
+document.querySelector('.menu').addEventListener('click', ()=>{
+    document.querySelector('body').classList.toggle('showsidebar')
+})
